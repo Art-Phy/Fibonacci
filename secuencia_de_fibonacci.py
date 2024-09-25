0936198f66c0cfdf1bfc5ffbfcd7ac32e1bd37bc
ArturoGit <arturosanma@gmail.com>
2024-09-25 17:15:31 +0200

    main added

---
+++ secuencia_de_fibonacci.py
@@ -0,0 +1,22 @@
+### Serie de Fibonacci ###
+
+# Se le pide al usuario cuántos números desea imprimir
+n = int(input("¿Cuántos números de la secuencia deseas? "))
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    cuenta = 0
+
+# Se crea un bucle hasta el número que haya escrito el usuario
+    while cuenta < n:
+        print(a, end=" ") # Se imprime el número actual
+
+        a, b = b, a + b
+        cuenta += 1 # Se le indica que cada sucesión es +1
+
+    # Este print se añade para evitar que al final de la impresión me salga un %
+    print()
+
+# Se llama a la función
+fibonacci(n)
